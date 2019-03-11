a637a54d6a8a986d5bec75bfd8ff6a7caf734e2b
Mickey K <mickeykkim@gmail.com>
2019-03-11 20:14:50 +0000

    Table tests complete v1)

--- Table.java
+++ Table.java
@@ -25,9 +25,19 @@ class Table {
    }
 
    void setColumnNames(String... names) {
+      this.columns.clear();
       for (String entry : names) {
          this.columns.add(entry);
+         //System.out.println(entry);
       }
+   }
+
+   String getColumnName(int idx) {
+      if (idx >= this.columns.size() || idx < 0) {
+            System.out.println("No such column in table.");
+            throw new IndexOutOfBoundsException();
+      }
+      return this.columns.get(idx);
    }
 
    String getName(){
@@ -45,34 +55,36 @@ class Table {
    void add(Record data) {
       if (data.size() != columns.size()) {
          System.out.println("Input data does not match table columns.");
-         throw new IndexOutOfBoundsException();
+         throw new IllegalArgumentException();
       }
       this.records.add(data);
    }
 
-   void checkRecordDataExists(int idx) {
+   boolean checkRecordDataExists(int idx) {
       if (idx >= this.records.size() || idx < 0) {
          System.out.println("No such record in table.");
          throw new IndexOutOfBoundsException();
       }
+      return true;
    }
 
-   void checkRecordInputSize(Record data) {
+   boolean checkRecordInputSize(Record data) {
       if (data.size() != this.columns.size()) {
          System.out.println("Input data does not match table columns.");
-         throw new IndexOutOfBoundsException();
+         throw new IllegalArgumentException();
       }
+      return true;
    }
 
    void insert(int idx, Record data) {
-      checkRecordDataExists(idx);
-      checkRecordInputSize(data);
+      if (!checkRecordDataExists(idx)) return;
+      if (!checkRecordInputSize(data)) return;
       this.records.add(idx, data);
       delete(idx + 1);
    }
 
    void delete(int idx) {
-      checkRecordDataExists(idx);
+      if (!checkRecordDataExists(idx)) return;
       this.records.remove(idx);
    }
 
@@ -88,12 +100,18 @@ class Table {
       PrintStream out = new PrintStream(baos);
       PrintStream console = System.out;
       System.setOut(out);
-      //table creation
+      //Begin tests
       Table test = new Table();
       assert(test.getName().equals("untitled"));
       String testName = "test";
-      Table test1 = new Table(testName);
-      assert(test1.getName().equals(testName));
+      Table test0 = new Table(testName);
+      assert(test0.getName().equals(testName));
+      //checking column validity
+      test0.setColumnNames("1", "2", "3", "4");
+      assert(test0.getColumnName(0).equals("1"));
+      assert(test0.getColumnName(1).equals("2"));
+      assert(test0.getColumnName(2).equals("3"));
+      assert(test0.getColumnName(3).equals("4"));
       //reset System.out
       System.out.flush();
       System.setOut(console);
@@ -105,6 +123,61 @@ class Table {
       PrintStream out = new PrintStream(baos);
       PrintStream console = System.out;
       System.setOut(out);
+      //Begin tests
+      //checking add records
+      Table test1 = new Table();
+      test1.setColumnNames("1", "2", "3", "4");
+      Record testR1 = new Record("a", "b", "c", "d");
+      test1.add(testR1);
+      assert(test1.select(0).getField(0).equals("a"));
+      assert(test1.select(0).getField(1).equals("b"));
+      assert(test1.select(0).getField(2).equals("c"));
+      assert(test1.select(0).getField(3).equals("d"));
+      Record testR2 = new Record("a", "b", "c");
+      boolean caught = false;
+      try { test1.add(testR2); } 
+      catch (IllegalArgumentException e) { caught = true; }
+      assert(caught == true);
+      caught = false;
+      //checking delete records
+      Record testR3 = new Record("q", "w", "e", "r");
+      test1.add(testR3);
+      test1.delete(0);
+      assert(test1.select(0).getField(0).equals("q"));
+      assert(test1.select(0).getField(1).equals("w"));
+      assert(test1.select(0).getField(2).equals("e"));
+      assert(test1.select(0).getField(3).equals("r"));
+      test1.delete(0);
+      try { test1.delete(0); }
+      catch (IndexOutOfBoundsException e) { caught = true; }
+      assert(caught == true);
+      caught = false;
+      //checking insert records
+      test1.add(testR1);
+      test1.add(testR1);
+      test1.insert(1, testR3);
+      assert(test1.select(1).getField(0).equals("q"));
+      assert(test1.select(1).getField(1).equals("w"));
+      assert(test1.select(1).getField(2).equals("e"));
+      assert(test1.select(1).getField(3).equals("r"));
+      try { test1.insert(4, testR3); }
+      catch (IndexOutOfBoundsException e) { caught = true; }
+      assert(caught == true);
+      caught = false;
+      //checking update records
+      test1.delete(0);
+      test1.delete(0);
+      test1.add(testR1);
+      test1.update(0, 0, "x");
+      assert(test1.select(0).getField(0).equals("x"));
+      try { test1.update(1, 0, "x"); }
+      catch (IndexOutOfBoundsException e) { caught = true; }
+      assert(caught == true);
+      caught = false;
+      try { test1.update(0, 4, "x"); }
+      catch (IndexOutOfBoundsException e) { caught = true; }
+      assert(caught == true);
+      caught = false;
       //reset System.out
       System.out.flush();
       System.setOut(console);
